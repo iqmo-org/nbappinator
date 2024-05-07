7625e84c5a2706d1e9a582a608ec429672cb8ade
paul@iqmo.com <paul@iqmo.com>
2024-05-07 08:47:24 -0400

    fix: Single source version field

--- nbappinator/__init__.py
+++ nbappinator/__init__.py
@@ -1,7 +1,9 @@
+from .version import __version__
 from .browser_title import BrowserTitle
 from .appinator import TabbedUiModel, UiModel, UiPage, SelectTypes, UiWidget
 
 __all__ = [
+    "__version__",
     "TabbedUiModel",
     "UiModel",
     "UiPage",
