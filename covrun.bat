733fe0cdfc2975e28839d8cf260e3648923bc604
Paul Timmins <paul@iqmo.com>
2024-05-22 15:21:51 -0400

    fix: rem not #

--- covrun.bat
+++ covrun.bat
@@ -3,5 +3,6 @@ set COVERAGE_PROCESS_START=%cd%\.coveragerc
 echo COVERAGE_PROCESS_START=%COVERAGE_PROCESS_START%
 coverage erase
 pytest --cov=nbappinator -n auto
-# coverage combine Not needed when running a single pytest-cov run
+
+rem coverage combine Not needed when running a single pytest-cov run
 coverage report -m
