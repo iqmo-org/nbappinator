e9bce8890f863f5d2b7c7ce07f6c3ec05a171df1
Paul Timmins <paul@iqmo.com>
2024-05-22 08:10:06 -0400

    test: When using a single pytest-cov run, don't need to combine

--- covrun.bat
+++ covrun.bat
@@ -3,5 +3,5 @@ set COVERAGE_PROCESS_START=%cd%\.coveragerc
 echo COVERAGE_PROCESS_START=%COVERAGE_PROCESS_START%
 coverage erase
 pytest --cov=nbappinator -n auto
-coverage combine
+# coverage combine Not needed when running a single pytest-cov run
 coverage report -m
